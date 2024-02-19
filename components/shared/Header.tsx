fb97870ca46aae26f638eeda3f728cab1c27cc50
satyamkumar420 <satyamkumar2460@gmail.com>
2024-02-19 22:27:11 +0530

    changes

---
+++ components/shared/Header.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Header = ({ title, subtitle }: { title: string; subtitle?: string }) => {
+  return (
+    <>
+      <h2 className="h2-bold text-gradient">{title}</h2>
+      {subtitle && <p className="p-16-regular mt-4">{subtitle}</p>}
+    </>
+  );
+};
+
+export default Header;
